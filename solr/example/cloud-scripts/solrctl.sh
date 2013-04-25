2c6538e8bc82293c6c10b68d5509eb2e4b6eebb1
Roman Shaposhnik <rvs@cloudera.com>
2013-04-24 17:26:29 -0700

    CDH-11743. solrctl has hard-coded path for zkcli.sh

--- solr/example/cloud-scripts/solrctl.sh
+++ solr/example/cloud-scripts/solrctl.sh
@@ -131,6 +131,8 @@ SOLR_ADMIN_CURL='curl -i --retry 5 -s -L -k'
 SOLR_ADMIN_CHAT=echo
 SOLR_ADMIN_API_CMD='solr_webapi'
 
+SOLR_HOME=${SOLR_HOME:-/usr/lib/solr/}
+
 # First eat up all the global options
 
 while test $# != 0 ; do
@@ -167,7 +169,7 @@ if [ -z "$SOLR_ZK_ENSEMBLE" ] ; then
 	If you running remotely, please use --zk zk_ensemble.
 	__EOT__
 else
-  SOLR_ADMIN_ZK_CMD='/usr/lib/solr/bin/zkcli.sh -zkhost $SOLR_ZK_ENSEMBLE 2>/dev/null'
+  SOLR_ADMIN_ZK_CMD='${SOLR_HOME}/bin/zkcli.sh -zkhost $SOLR_ZK_ENSEMBLE 2>/dev/null'
 fi
 
 
@@ -263,7 +265,7 @@ while test $# != 0 ; do
 
             mkdir -p "$3" > /dev/null 2>&1
             [ -d "$3" ] || usage "Error: $3 has to be a directory"
-            cp -r /usr/lib/solr/coreconfig-template/* "$3"
+            cp -r ${SOLR_HOME}/coreconfig-template/* "$3"
             shift 3
             ;;
         --list)
