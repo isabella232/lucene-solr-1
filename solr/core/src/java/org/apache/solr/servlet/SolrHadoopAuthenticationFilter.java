cb8993e2f60a19fa407c340c29f66270d57f1007
Gregory Chanan <gchanan@cloudera.com>
2013-07-17 14:35:03 -0700

    CDH-13060: Add Kerberos AuthenticationFilter to Solr via the web.xml

---
+++ solr/core/src/java/org/apache/solr/servlet/SolrHadoopAuthenticationFilter.java
@@ -0,0 +1,107 @@
+package org.apache.solr.servlet;
+/**
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements. See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership. The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License. You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import org.apache.hadoop.security.authentication.server.AuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Properties;
+import org.apache.hadoop.security.authentication.server.PseudoAuthenticationHandler;
+import org.apache.solr.servlet.TeeHttpServletRequestWrapper;
+
+/**
+ * Authentication filter that extends Hadoop-auth AuthenticationFilter to override
+ * the configuration loading.
+ */
+public class SolrHadoopAuthenticationFilter extends AuthenticationFilter {
+  static final String SOLR_PREFIX = "solr.authentication.";
+
+  /**
+   * Returns the System properties to be used by the authentication filter.
+   * <p/>
+   * All properties from the System properties with names that starts with {@link #SOLR_PREFIX} will
+   * be returned. The keys of the returned properties are trimmed from the {@link #SOLR_PREFIX}
+   * prefix, for example the property name 'solr.authentication.type' will
+   * be just 'type'.
+   *
+   * @param configPrefix configuration prefix, this parameter is ignored by this implementation.
+   * @param filterConfig filter configuration, this parameter is ignored by this implementation.
+   * @return all System properties prefixed with {@link #SOLR_PREFIX}, without the
+   * prefix.
+   */
+  @Override
+  protected Properties getConfiguration(String configPrefix, FilterConfig filterConfig) {
+    Properties props = new Properties();
+
+    //setting the cookie path to root '/' so it is used for all resources.
+    props.setProperty(AuthenticationFilter.COOKIE_PATH, "/");
+
+    for (String name : System.getProperties().stringPropertyNames()) {
+      if (name.startsWith(SOLR_PREFIX)) {
+        String value = System.getProperty(name);
+        name = name.substring(SOLR_PREFIX.length());
+        props.setProperty(name, value);
+      }
+    }
+
+    // ensure old behavior (no authentication ) if properties not specified
+    if (props.getProperty(AUTH_TYPE) == null) {
+      props.setProperty(AUTH_TYPE, PseudoAuthenticationHandler.TYPE);
+      if (props.getProperty(PseudoAuthenticationHandler.ANONYMOUS_ALLOWED) == null) {
+        props.setProperty(PseudoAuthenticationHandler.ANONYMOUS_ALLOWED, "true");
+      }
+    }
+
+    return props;
+  }
+
+  /**
+   * Enforces authentication using Hadoop-auth AuthenticationFilter.
+   *
+   * @param request http request.
+   * @param response http response.
+   * @param filterChain filter chain.
+   * @throws IOException thrown if an IO error occurs.
+   * @throws ServletException thrown if a servlet error occurs.
+   */
+  @Override
+  public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain filterChain)
+      throws IOException, ServletException {
+
+    FilterChain filterChainWrapper = new FilterChain() {
+      @Override
+      public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse)
+          throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+        filterChain.doFilter(servletRequest, servletResponse);
+      }
+    };
+
+    // Ensure the next filter in the chain (i.e. SolrRequestFilter) gets an
+    // InputStream with all the content, as that is required.
+    TeeHttpServletRequestWrapper teeRequest =
+      new TeeHttpServletRequestWrapper((HttpServletRequest)request);
+    super.doFilter(teeRequest, response, filterChainWrapper);
+  }
+}
---
+++ solr/core/src/test/org/apache/solr/servlet/TeeHttpServletRequestWrapperTest.java
@@ -0,0 +1,61 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.solr.servlet;
+
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.ServletInputStream;
+import org.apache.commons.io.IOUtils;
+import org.apache.solr.SolrTestCaseJ4;
+import org.junit.Test;
+
+public class TeeHttpServletRequestWrapperTest extends SolrTestCaseJ4 {
+
+  @Test
+  public void testTeeInputStream() throws Exception {
+    HttpServletRequest request = createMock(HttpServletRequest.class);
+    byte [] foobar = "foobar".getBytes();
+    final InputStream inputStream = new ByteArrayInputStream(foobar);
+    final ServletInputStream sis = new ServletInputStream() {
+      public int read() throws IOException {
+        return inputStream.read();
+      }
+    };
+    expect(request.getInputStream()).andReturn(sis).anyTimes();
+    replay(request);
+    TeeHttpServletRequestWrapper tee = new TeeHttpServletRequestWrapper(request);
+
+    // read part of the first inputStream
+    int firstReadLength = foobar.length / 2;
+    InputStream firstInputStream = tee.getInputStream();
+    for (int i = 0; i < firstReadLength; ++i) {
+      firstInputStream.read();
+    }
+
+    // Ensure the second called to getInputStream contains the full stream
+    assertTrue(Arrays.equals(foobar, IOUtils.toByteArray(tee.getInputStream())));
+  }
+}
