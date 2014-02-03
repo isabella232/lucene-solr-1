cfc2fb802e2956722103b3e698ec7e1e107c0801
Mark Miller <markrmiller@gmail.com>
2014-02-03 13:55:10 -0500

    CDH-12066: backport SOLR-5657: When a SolrCore starts on HDFS, it should gracefully handle HDFS being in SafeMode.

--- solr/core/src/test/org/apache/solr/cloud/hdfs/BasicHdfsTest.java
+++ solr/core/src/test/org/apache/solr/cloud/hdfs/BasicHdfsTest.java
@@ -20,15 +20,20 @@ package org.apache.solr.cloud.hdfs;
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hdfs.MiniDFSCluster;
+import org.apache.hadoop.hdfs.server.namenode.NameNodeAdapter;
+import org.apache.lucene.util.LuceneTestCase;
 import org.apache.lucene.util.LuceneTestCase.Slow;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.request.QueryRequest;
 import org.apache.solr.cloud.BasicDistributedZkTest;
+import org.apache.solr.cloud.ChaosMonkey;
 import org.apache.solr.common.params.CollectionParams.CollectionAction;
 import org.apache.solr.common.params.ModifiableSolrParams;
 import org.apache.solr.common.util.NamedList;
@@ -41,8 +46,11 @@ import com.carrotsearch.randomizedtesting.annotations.ThreadLeakScope.Scope;
 @Slow
 @ThreadLeakScope(Scope.NONE) // hdfs client currently leaks thread(s)
 public class BasicHdfsTest extends BasicDistributedZkTest {
-
+  private static final String DELETE_DATA_DIR_COLLECTION = "delete_data_dir";
+  
   private static MiniDFSCluster dfsCluster;
+  
+  private boolean testRestartIntoSafeMode;
   
   @BeforeClass
   public static void setupClass() throws Exception {
@@ -70,6 +78,7 @@ public class BasicHdfsTest extends BasicDistributedZkTest {
     super();
     sliceCount = 1;
     shardCount = 1;
+    testRestartIntoSafeMode = random().nextBoolean();
   }
   
   protected String getSolrXml() {
@@ -78,9 +87,9 @@ public class BasicHdfsTest extends BasicDistributedZkTest {
   
   @Override
   public void doTest() throws Exception {
-    createCollection("delete_data_dir", 1, 1, 1);
-    waitForRecoveriesToFinish("delete_data_dir", false);
-    cloudClient.setDefaultCollection("delete_data_dir");
+    createCollection(DELETE_DATA_DIR_COLLECTION, 1, 1, 1);
+    waitForRecoveriesToFinish(DELETE_DATA_DIR_COLLECTION, false);
+    cloudClient.setDefaultCollection(DELETE_DATA_DIR_COLLECTION);
     cloudClient.getZkStateReader().updateClusterState(true);
     NamedList<Object> response = cloudClient.query(
         new SolrQuery().setRequestHandler("/admin/system")).getResponse();
@@ -90,7 +99,7 @@ public class BasicHdfsTest extends BasicDistributedZkTest {
 
     ModifiableSolrParams params = new ModifiableSolrParams();
     params.set("action", CollectionAction.DELETE.toString());
-    params.set("name", "delete_data_dir");
+    params.set("name", DELETE_DATA_DIR_COLLECTION);
     QueryRequest request = new QueryRequest(params);
     request.setPath("/admin/collections");
     cloudClient.request(request);
@@ -102,5 +111,30 @@ public class BasicHdfsTest extends BasicDistributedZkTest {
         "Data directory exists after collection removal : "
             + dataDir, fs.exists(new Path(dataDir)));
     fs.close();
+    
+    if (testRestartIntoSafeMode) {
+      createCollection(DELETE_DATA_DIR_COLLECTION, 1, 1, 1);
+      
+      waitForRecoveriesToFinish(DELETE_DATA_DIR_COLLECTION, false);
+      
+      ChaosMonkey.stop(jettys.get(0));
+      
+      // enter safe mode and restart a node
+      NameNodeAdapter.enterSafeMode(dfsCluster.getNameNode(), false);
+      
+      int rnd = LuceneTestCase.random().nextInt(10000);
+      Timer timer = new Timer();
+      timer.schedule(new TimerTask() {
+        
+        @Override
+        public void run() {
+          NameNodeAdapter.leaveSafeMode(dfsCluster.getNameNode());
+        }
+      }, rnd);
+      
+      ChaosMonkey.start(jettys.get(0));
+      
+      waitForRecoveriesToFinish(DELETE_DATA_DIR_COLLECTION, false);
+    }
   }
 }
