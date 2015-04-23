e5ce6471c055496cf70ed4e10528808b989f3e8b
Mark Miller <markrmiller@gmail.com>
2015-04-23 14:23:24 -0400

    CDH-15931: Backport SOLR-7437 Make HDFS transaction log replication factor configurable.

--- solr/core/src/java/org/apache/solr/update/HdfsTransactionLog.java
+++ solr/core/src/java/org/apache/solr/update/HdfsTransactionLog.java
@@ -67,11 +67,11 @@ public class HdfsTransactionLog extends TransactionLog {
 
   private volatile boolean isClosed = false;
 
-  HdfsTransactionLog(FileSystem fs, Path tlogFile, Collection<String> globalStrings) {
-    this(fs, tlogFile, globalStrings, false);
+  HdfsTransactionLog(FileSystem fs, Path tlogFile, Collection<String> globalStrings, Integer tlogDfsReplication) {
+    this(fs, tlogFile, globalStrings, false, tlogDfsReplication);
   }
 
-  HdfsTransactionLog(FileSystem fs, Path tlogFile, Collection<String> globalStrings, boolean openExisting) {
+  HdfsTransactionLog(FileSystem fs, Path tlogFile, Collection<String> globalStrings, boolean openExisting, Integer tlogDfsReplication) {
     super();
     boolean success = false;
     this.fs = fs;
@@ -94,7 +94,7 @@ public class HdfsTransactionLog extends TransactionLog {
       } else {
         fs.delete(tlogFile, false);
         
-        tlogOutStream = fs.create(tlogFile, (short)1);
+        tlogOutStream = fs.create(tlogFile, (short)tlogDfsReplication.intValue());
         tlogOutStream.hsync();
       }
 
