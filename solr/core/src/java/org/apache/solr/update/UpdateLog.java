e16d1beacf5c0c2ab582c887fc108461bef3edd6
Mark Miller <markrmiller@gmail.com>
2015-04-28 09:17:41 -0400

    CDH-27374: Backport SOLR-7478 UpdateLog#close shuts down it's executor with interrupts before running close, possibly preventing a clean close.

--- solr/core/src/java/org/apache/solr/update/UpdateLog.java
+++ solr/core/src/java/org/apache/solr/update/UpdateLog.java
@@ -847,11 +847,7 @@ public class UpdateLog implements PluginInfoInitialized {
   
   public void close(boolean committed, boolean deleteOnClose) {
     synchronized (this) {
-      try {
-        ExecutorUtil.shutdownNowAndAwaitTermination(recoveryExecutor);
-      } catch (Exception e) {
-        SolrException.log(log, e);
-      }
+      recoveryExecutor.shutdown(); // no new tasks
 
       // Don't delete the old tlogs, we want to be able to replay from them and retrieve old versions
 
@@ -865,6 +861,11 @@ public class UpdateLog implements PluginInfoInitialized {
         log.forceClose();
       }
 
+      try {
+        ExecutorUtil.shutdownNowAndAwaitTermination(recoveryExecutor);
+      } catch (Exception e) {
+        SolrException.log(log, e);
+      }
     }
   }
 
