markrmiller/lucene-solr-svn2git

SOLR-6146: Incorrect configuration such as wrong chroot in zk server address can cause CloudSolrServer to leak resources
git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_4x@1601622 13f79535-47bb-0310-9956-ffa450edef68

solr/solrj/src/java/org/apache/solr/client/solrj/impl/CloudSolrServer.java
@@ -236,24 +236,33 @@ public class CloudSolrServer extends SolrServer {
     if (zkStateReader == null) {
       synchronized (this) {
         if (zkStateReader == null) {
+          ZkStateReader zk = null;
           try {
-            ZkStateReader zk = new ZkStateReader(zkHost, zkClientTimeout,
+            zk = new ZkStateReader(zkHost, zkClientTimeout,
                 zkConnectTimeout);
             zk.createClusterStateWatchersAndUpdate();
             zkStateReader = zk;
           } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
+            if (zk != null) zk.close();
             throw new ZooKeeperException(SolrException.ErrorCode.SERVER_ERROR,
                 "", e);
           } catch (KeeperException e) {
+            if (zk != null) zk.close();
             throw new ZooKeeperException(SolrException.ErrorCode.SERVER_ERROR,
                 "", e);
           } catch (IOException e) {
+            if (zk != null) zk.close();
             throw new ZooKeeperException(SolrException.ErrorCode.SERVER_ERROR,
                 "", e);
           } catch (TimeoutException e) {
+            if (zk != null) zk.close();
             throw new ZooKeeperException(SolrException.ErrorCode.SERVER_ERROR,
                 "", e);
+          } catch (Exception e) {
+            if (zk != null) zk.close();
+            // do not wrap because clients may be relying on the underlying exception being thrown
+            throw e;
           }
         }
       }

isabella232/lucene-solr-1

(cherry picked from commit be5b83d79e08a055a76aabf9afc3aa50e3b319c3)
Ref: CDH-29299
Conflicts:
	solr/CHANGES.txt
	solr/solrj/src/java/org/apache/solr/client/solrj/impl/CloudSolrServer.java
	solr/solrj/src/test/org/apache/solr/client/solrj/impl/CloudSolrServerTest.java

solr/solrj/src/java/org/apache/solr/client/solrj/impl/CloudSolrServer.java
@@ -147,24 +147,33 @@ public class CloudSolrServer extends SolrServer {
     if (zkStateReader == null) {
       synchronized (this) {
         if (zkStateReader == null) {
+          ZkStateReader zk = null;
           try {
-            ZkStateReader zk = new ZkStateReader(zkHost, zkConnectTimeout,
+            zk = new ZkStateReader(zkHost, zkConnectTimeout,
                 zkClientTimeout);
             zk.createClusterStateWatchersAndUpdate();
             zkStateReader = zk;
           } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
+            if (zk != null) zk.close();
             throw new ZooKeeperException(SolrException.ErrorCode.SERVER_ERROR,
                 "", e);
           } catch (KeeperException e) {
+            if (zk != null) zk.close();
             throw new ZooKeeperException(SolrException.ErrorCode.SERVER_ERROR,
                 "", e);
           } catch (IOException e) {
+            if (zk != null) zk.close();
             throw new ZooKeeperException(SolrException.ErrorCode.SERVER_ERROR,
                 "", e);
           } catch (TimeoutException e) {
+            if (zk != null) zk.close();
             throw new ZooKeeperException(SolrException.ErrorCode.SERVER_ERROR,
                 "", e);
+          } catch (Exception e) {
+            if (zk != null) zk.close();
+            // do not wrap because clients may be relying on the underlying exception being thrown
+            throw e;
           }
         }
       }