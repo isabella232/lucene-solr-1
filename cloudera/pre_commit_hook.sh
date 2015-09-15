f059a8fede6d7e838a0a8f186954a36236269b85
Gregory Chanan <gchanan@cloudera.com>
2015-09-15 13:55:43 -0700

    Fix documentation-lint problems, enable "ant precommit" in precommit hook.
    Change-Id: I8d9a7ccb758a93591d1e2672cef4c53c64a9f6ac
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/8197
    Tested-by: jenkins
    Reviewed-by: Greg Chanan <gchanan@cloudera.com>

--- cloudera/pre_commit_hook.sh
+++ cloudera/pre_commit_hook.sh
@@ -2,12 +2,11 @@
 set -ex
 export JAVA7_BUILD=true
 source /opt/toolchain/toolchain.sh
- BUILD_OPTS="-Dversion=${FULL_VERSION}                                  \
-            -Dslf4j.binding=slf4j-log4j12 -Dexclude.from.war=nothing   \
+ BUILD_OPTS="-Dslf4j.binding=slf4j-log4j12 -Dexclude.from.war=nothing   \
             -Divy.home=${HOME}/.ivy2 -Drepo.maven.org=$IVY_MIRROR_PROP \
             -Divy_install_path=${HOME}/.ant/lib -lib ${HOME}/.ant/lib  \
             -Dreactor.repo=file://${HOME}/.m2/repository"
 
 ant $BUILD_OPTS ivy-bootstrap
-ant $BUILD_OPTS clean compile compile-test validate
+ant $BUILD_OPTS clean compile compile-test precommit
 echo "Pass! "
