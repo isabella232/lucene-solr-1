88616b6135bb85470aa351100d020ccb7d2d0c1b
Christine Poerschke <cpoerschke@apache.org>
2015-11-05 17:03:49 +0000

    SOLR-8223: Avoid accidentally swallowing OutOfMemoryError (in LeaderInitiatedRecoveryThread.java or CoreContainer.java)
    Description: Need to allow OOME to propogate to kill scripts.
    Author: Christine Poerschke <cpoerschke@apache.org>
    Ref: CDH-33768
    
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1712817 13f79535-47bb-0310-9956-ffa450edef68
    (cherry picked from commit 4e16518bdd811273133dc70d92fe9bd557959ed7)
    
    Conflicts:
    	solr/CHANGES.txt
    	solr/core/src/java/org/apache/solr/core/CoreContainer.java
    
    Change-Id: I644caf42990bb8fc834de41eac0f7bdc5c9cac69
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/9188
    Tested-by: jenkins
    Reviewed-by: Greg Chanan <gchanan@cloudera.com>
    (cherry picked from commit 9336d5659bbebaa86d6af85eef1e4856135215e0)
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/9355

--- solr/core/src/java/org/apache/solr/core/CoreContainer.java
+++ solr/core/src/java/org/apache/solr/core/CoreContainer.java
@@ -287,8 +287,8 @@ public class CoreContainer {
         for (SolrCore core : cores) {
           try {
             zkSys.registerInZk(core, true);
-          } catch (Throwable t) {
-            SolrException.log(log, "Error registering SolrCore", t);
+          } catch (RuntimeException e) {
+            SolrException.log(log, "Error registering SolrCore", e);
           }
         }
       }
