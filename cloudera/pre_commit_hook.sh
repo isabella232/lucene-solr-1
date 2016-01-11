0b75296dac75a2072f7a443461981f857c2fd1ee
Gregory Chanan <gchanan@cloudera.com>
2016-01-11 14:08:56 -0800

    CDH-36079: Add skipRegexChecksum to solr precommit checks
    Change-Id: Iee456cc9eaf91aa02049c0366a29a8e24b559dc9
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/10181
    Tested-by: jenkins
    Reviewed-by: Mark Miller <markmiller@cloudera.com>

--- cloudera/pre_commit_hook.sh
+++ cloudera/pre_commit_hook.sh
@@ -5,7 +5,8 @@ source /opt/toolchain/toolchain.sh
  BUILD_OPTS="-Dslf4j.binding=slf4j-log4j12 -Dexclude.from.war=nothing   \
             -Divy.home=${HOME}/.ivy2 -Drepo.maven.org=$IVY_MIRROR_PROP \
             -Divy_install_path=${HOME}/.ant/lib -lib ${HOME}/.ant/lib  \
-            -Dreactor.repo=file://${HOME}/.m2/repository"
+            -Dreactor.repo=file://${HOME}/.m2/repository \
+            -DskipRegexChecksum='.*-cdh5-.*'"
 
 ant $BUILD_OPTS ivy-bootstrap
 ant $BUILD_OPTS clean compile compile-test precommit
