24368e7de532a3a4b67a8a3986f7525d6a4b6a99
Gregory Chanan <gchanan@cloudera.com>
2016-01-14 15:09:07 -0800

    CDH-36253: CLOUDERA-BUILD. Change precommit hook to use local javadoc url and fix skip checksum regex
    Change-Id: I22bcb19862f41ae182842b8fecdde6825fec652c
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/10262
    Tested-by: jenkins
    Reviewed-by: Mark Miller <markmiller@cloudera.com>

--- cloudera/pre_commit_hook.sh
+++ cloudera/pre_commit_hook.sh
@@ -6,7 +6,8 @@ source /opt/toolchain/toolchain.sh
             -Divy.home=${HOME}/.ivy2 -Drepo.maven.org=$IVY_MIRROR_PROP \
             -Divy_install_path=${HOME}/.ant/lib -lib ${HOME}/.ant/lib  \
             -Dreactor.repo=file://${HOME}/.m2/repository \
-            -DskipRegexChecksum='.*-cdh5-.*'"
+            -DskipRegexChecksum=.*-cdh5\..* \
+            -DuseLocalJavadocUrl=true"
 
 ant $BUILD_OPTS ivy-bootstrap
 ant $BUILD_OPTS clean compile compile-test precommit
