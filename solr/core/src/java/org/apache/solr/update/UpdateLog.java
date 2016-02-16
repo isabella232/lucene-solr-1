cb99c81788614c44696978ddea01ca13ca93e5eb
Mike Drob <mdrob@cloudera.com>
2016-02-16 16:40:27 -0500

    SOLR-8575 Addendum to Fix HDFSLogReader replay
    Description: Additional fixes to cover an hdfs tlog data integrity bug.
    Author: Mark Miller
    Ref: CDH-30882
    
    Conflicts:
    	solr/CHANGES.txt
    	solr/core/src/java/org/apache/solr/update/UpdateLog.java
    	solr/core/src/java/org/apache/solr/util/TestInjection.java
            solr/core/src/test/org/apache/solr/cloud/ChaosMonkeyNothingIsSafeTest.java
            solr/core/src/test/org/apache/solr/cloud/TlogReplayBufferedWhileIndexingTest.java
    	solr/core/src/test/org/apache/solr/cloud/hdfs/HdfsChaosMonkeyNothingIsSafeTest.java
            solr/core/src/test/org/apache/solr/cloud/hdfs/HdfsTlogReplayBufferedWhileIndexingTest.java
            solr/solrj/src/java/org/apache/solr/common/NonExistentCoreException.java
    
    Change-Id: I81d5c5a6e4e9054e9b8d58adca888250440505f8
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/11035
    Tested-by: jenkins
    Tested-by: Mike Drob <mdrob@cloudera.com>
    Reviewed-by: Mark Miller <markmiller@cloudera.com>

--- solr/core/src/java/org/apache/solr/update/UpdateLog.java
+++ solr/core/src/java/org/apache/solr/update/UpdateLog.java
@@ -62,6 +62,7 @@ import org.apache.solr.update.processor.UpdateRequestProcessor;
 import org.apache.solr.update.processor.UpdateRequestProcessorChain;
 import org.apache.solr.util.DefaultSolrThreadFactory;
 import org.apache.solr.util.RefCounted;
+import org.apache.solr.util.TestInjection;
 import org.apache.solr.util.plugin.PluginInfoInitialized;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -1291,7 +1292,7 @@ public class UpdateLog implements PluginInfoInitialized {
                         "log replay status {} active={} starting pos={} current pos={} current size={} % read={}",
                         translog, activeLog, recoveryInfo.positionOfStart, cpos, csize,
                         Math.floor(cpos / (double) csize * 100.));
-                
+
               }
             }
 
@@ -1413,6 +1414,7 @@ public class UpdateLog implements PluginInfoInitialized {
             loglog.warn("REPLAY_ERR: Exception replaying log", ex);
             // something wrong with the request?
           }
+          assert TestInjection.injectUpdateLogReplayRandomPause();
         }
 
         CommitUpdateCommand cmd = new CommitUpdateCommand(req, false);
