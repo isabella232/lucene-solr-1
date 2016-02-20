8ba86fc2013ac9de27f9f4c073797c66e3936dcd
markrmiller <markrmiller@apache.org>
2016-02-20 12:19:34 -0500

    CDH-37417: SOLR-8683 Always consume the full request on the server, not just in the case of an error.
    Change-Id: I0fe7670bbabf7172daf55e01c48d85fd33f27232
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/11197
    Tested-by: jenkins
    Reviewed-by: Yonik Seeley <yonik@cloudera.com>
    Reviewed-by: Wolfgang Hoschek <whoschek@cloudera.com>
    (cherry picked from commit 1f8c4058b5dd50c670ca7658274135b0d72d97e3)
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/12110
    Tested-by: Jenkins User
    Reviewed-by: Hrishikesh Gadre <hgadre@cloudera.com>

--- solr/core/src/java/org/apache/solr/servlet/SolrDispatchFilter.java
+++ solr/core/src/java/org/apache/solr/servlet/SolrDispatchFilter.java
@@ -213,6 +213,14 @@ public class SolrDispatchFilter extends BaseSolrFilter {
   }
   
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain, boolean retry) throws IOException, ServletException {
+    try {
+      httpSolrCall(request, response, chain, retry);
+    } finally {
+      consumeInput(request);
+    }
+  }
+  
+  public void httpSolrCall(ServletRequest request, ServletResponse response, FilterChain chain, boolean retry) throws IOException, ServletException {
     if (abortErrorMessage != null) {
       sendError(request, response, 500, abortErrorMessage);
       return;
@@ -472,14 +480,10 @@ public class SolrDispatchFilter extends BaseSolrFilter {
   }
 
   public void sendError(ServletRequest request, ServletResponse response, int errorCode, String message) throws IOException {
-    try {
-      if (message != null) {
-        ((HttpServletResponse) response).sendError(errorCode, message);
-      } else {
-        ((HttpServletResponse) response).sendError(errorCode);
-      }
-    } finally {
-      consumeInput(request);
+    if (message != null) {
+      ((HttpServletResponse) response).sendError(errorCode, message);
+    } else {
+      ((HttpServletResponse) response).sendError(errorCode);
     }
   }
   
@@ -793,11 +797,10 @@ public class SolrDispatchFilter extends BaseSolrFilter {
   private void writeResponse(SolrQueryResponse solrRsp, ServletRequest request, ServletResponse response,
                              QueryResponseWriter responseWriter, SolrQueryRequest solrReq, Method reqMethod)
           throws IOException {
-    try {
     // Now write it out
     final String ct = responseWriter.getContentType(solrReq, solrRsp);
     // don't call setContentType on null
-    if (null != ct) response.setContentType(ct); 
+    if (null != ct) response.setContentType(ct);
 
     if (solrRsp.getException() != null) {
       NamedList info = new SimpleOrderedMap();
@@ -813,19 +816,14 @@ public class SolrDispatchFilter extends BaseSolrFilter {
       } else {
         String charset = ContentStreamBase.getCharsetFromContentType(ct);
         Writer out = (charset == null)
-          ? new OutputStreamWriter(response.getOutputStream(), UTF8)
-          : new OutputStreamWriter(response.getOutputStream(), charset);
+            ? new OutputStreamWriter(response.getOutputStream(), UTF8)
+            : new OutputStreamWriter(response.getOutputStream(), charset);
         out = new FastWriter(out);
         responseWriter.write(out, solrReq, solrRsp);
         out.flush();
       }
     }
     //else http HEAD request, nothing to write out, waited this long just to get ContentType
-    } finally {
-      if (solrRsp.getException() != null) {
-        consumeInput(request);
-      }
-    }
   }
   
   protected void execute( HttpServletRequest req, SolrRequestHandler handler, SolrQueryRequest sreq, SolrQueryResponse rsp) {
@@ -875,13 +873,9 @@ public class SolrDispatchFilter extends BaseSolrFilter {
     } finally {
       try {
         if (exp != null) {
-          try {
-            SimpleOrderedMap info = new SimpleOrderedMap();
-            int code = ResponseUtils.getErrorInfo(ex, info, log);
-            response.sendError(code, info.toString());
-          } finally {
-            consumeInput(request);
-          }
+          SimpleOrderedMap info = new SimpleOrderedMap();
+          int code = ResponseUtils.getErrorInfo(ex, info, log);
+          response.sendError(code, info.toString());
         }
       } finally {
         if (core == null && localCore != null) {
