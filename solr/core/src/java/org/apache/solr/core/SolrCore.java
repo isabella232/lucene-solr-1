b366488cd3d76c9ffc3ae97526cbe80da72b8bdf
markrmiller <markrmiller@apache.org>
2016-02-25 09:31:39 -0500

    CDH-37459: SOLR-7332 Initialize the highest value for all version buckets with the max value from the index or recent updates to avoid unnecessary lookups to the index to check for reordered updates when processing new documents.
    Change-Id: I9e54ba2697e7288efddabf2dca24262e5d1b616a
    
    Conflicts:
    	solr/core/src/java/org/apache/solr/update/UpdateLog.java
    	solr/solrj/src/java/org/apache/solr/common/cloud/Replica.java

--- solr/core/src/java/org/apache/solr/core/SolrCore.java
+++ solr/core/src/java/org/apache/solr/core/SolrCore.java
@@ -1764,8 +1764,12 @@ public final class SolrCore implements SolrInfoMBean, Closeable {
             }
           });
         }
-        
-        if (currSearcher == null && firstSearcherListeners.size() > 0) {
+
+        if (currSearcher == null) {
+          if (updateHandler != null && updateHandler.getUpdateLog() != null) {
+            updateHandler.getUpdateLog().onFirstSearcher(newSearcher);
+          }
+
           future = searcherExecutor.submit(new Callable() {
             @Override
             public Object call() throws Exception {
