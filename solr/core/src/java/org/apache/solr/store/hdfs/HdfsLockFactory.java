markrmiller/lucene-solr-svn2git

SOLR-8351: Improve HdfsDirectory toString representation
git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1717344 13f79535-47bb-0310-9956-ffa450edef68

solr/core/src/java/org/apache/solr/store/hdfs/HdfsLockFactory.java
@@ -120,5 +120,10 @@ public class HdfsLockFactory extends LockFactory {
     public void ensureValid() throws IOException {
       // no idea how to implement this on HDFS
     }
+
+    @Override
+    public String toString() {
+      return "HdfsLock(lockFile=" + lockFile + ")";
+    }
   }
 }

isabella232/lucene-solr-1

Author: Mike Drob <mdrob@cloudera.com>
Ref: CDH-35282
Conflicts:
	solr/core/src/java/org/apache/solr/store/hdfs/HdfsLockFactory.java
Change-Id: Ib7a225d128a3f761c636c3a42b9135df9b680704
Reviewed-on: http://gerrit.sjc.cloudera.com:8080/9746
Tested-by: jenkins
Reviewed-by: Greg Chanan <gchanan@cloudera.com>
(cherry picked from commit 3261693791f84a2ac207b353d3cf62c462c132b3)
Reviewed-on: http://gerrit.sjc.cloudera.com:8080/11413
Reviewed-by: Mike Drob <mdrob@cloudera.com>

solr/core/src/java/org/apache/solr/store/hdfs/HdfsLockFactory.java
@@ -171,7 +171,7 @@ public class HdfsLockFactory extends LockFactory {
         IOUtils.closeQuietly(fs);
       }
     }
-    
+
     @Override
     public boolean isLocked() throws IOException {
       boolean isLocked = false;
@@ -183,7 +183,11 @@ public class HdfsLockFactory extends LockFactory {
       }
       return isLocked;
     }
-    
+
+    @Override
+    public String toString() {
+      return "HdfsLock(lockFile=" + lockPath + ")";
+    }
   }
   
 }