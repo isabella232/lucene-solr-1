289c3729faec601162021659d00e6a26dd4451df
Mike Drob <mdrob@cloudera.com>
2016-02-25 20:48:51 -0800

    Revert "SOLR-8351: Improve HdfsDirectory toString representation"
    This reverts commit 3261693791f84a2ac207b353d3cf62c462c132b3.
    
    Change-Id: Ib9fd02a67b8385c30a2ca8f13e0b2fa4247d1132
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/11455
    Tested-by: jenkins
    Reviewed-by: Mike Drob <mdrob@cloudera.com>

--- solr/core/src/java/org/apache/solr/store/hdfs/HdfsDirectory.java
+++ solr/core/src/java/org/apache/solr/store/hdfs/HdfsDirectory.java
@@ -262,9 +262,4 @@ public class HdfsDirectory extends BaseDirectory {
     }
     return this.hdfsDirPath.equals(((HdfsDirectory) obj).hdfsDirPath);
   }
-
-  @Override
-  public String toString() {
-    return this.getClass().getSimpleName() + "@" + hdfsDirPath + " lockFactory=" + lockFactory;
-  }
 }
