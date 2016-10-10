ad005daeb8df737b19a0d8b46b418c0f9058e91a
markrmiller <markrmiller@apache.org>
2016-10-10 10:15:15 -0400

    CLOUDERA_BUILD: cloudera directory from cdh5 as of 20160512
    Change-Id: I757fe3f0fbb030aa376d1fc566e4693ceaceb646

---
+++ cloudera/pre_commit_hook.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ex
+export JAVA7_BUILD=true
+source /opt/toolchain/toolchain.sh
+ BUILD_OPTS="-Dslf4j.binding=slf4j-log4j12 -Dexclude.from.war=nothing   \
+            -Divy.home=${HOME}/.ivy2 -Drepo.maven.org=$IVY_MIRROR_PROP \
+            -Divy_install_path=${HOME}/.ant/lib -lib ${HOME}/.ant/lib  \
+            -Dreactor.repo=file://${HOME}/.m2/repository \
+            -DskipRegexChecksum=.*-cdh5\..* \
+            -DuseLocalJavadocUrl=true"
+
+ant $BUILD_OPTS ivy-bootstrap
+ant $BUILD_OPTS clean compile compile-test precommit
+echo "Pass! "
