c333ff31e454b86e6be7a55faba48bd21548ec60
Mano Kovacs <manokovacs@cloudera.com>
2017-06-19 16:09:41 +0200

    [CDH-55371] Fixing license validation
    Change-Id: I6e4d0cc429e3fb8c74215da5c3628b6b60325658

--- solr/core/src/test/org/apache/solr/servlet/JMXJsonServletTest.java
+++ solr/core/src/test/org/apache/solr/servlet/JMXJsonServletTest.java
@@ -1,9 +1,4 @@
-package org.apache.solr.servlet;
-
 /*
- *  CLOUDERA ONLY
- *
- *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -11,14 +6,19 @@ package org.apache.solr.servlet;
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ *
+ * This is a verbatim copy of org.apache.hadoop.jmx.JmxJsonServlet.java
  */
+
+package org.apache.solr.servlet;
 
 import java.io.IOException;
 import java.io.StringWriter;
