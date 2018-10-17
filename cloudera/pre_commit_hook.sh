a15622c109026e6508574a991280aa17a0b3a9e4
Peter Somogyi <psomogyi@cloudera.com>
2018-10-17 19:21:08 +0200

    CDH-74252 Clean up license files and checksums
    Modify pre_commit_hook to reflect cdh6 jars
    Remove SNAPSHOT checksums
    Remove leftover license and notice files from removed dependencies
    
    Change-Id: I3893b63f0bea0a900721f00fc219f1035f239b01
    Reviewed-on: https://gerrit.sjc.cloudera.com/38223
    Tested-by: Jenkins User <jenkins@cloudera.com>
    CDH-Build: Jenkins User <jenkins@cloudera.com>
    Reviewed-by: Peter Cseh <gezapeti@cloudera.com>
    Quasar-L0: Peter Somogyi <psomogyi@cloudera.com>

--- cloudera/pre_commit_hook.sh
+++ cloudera/pre_commit_hook.sh
@@ -4,7 +4,7 @@ BUILD_OPTS="-Dslf4j.binding=slf4j-log4j12 -Dexclude.from.war=nothing   \
             -Divy.home=${HOME}/.ivy2 -Drepo.maven.org=$IVY_MIRROR_PROP \
             -Divy_install_path=${HOME}/.ant/lib -lib ${HOME}/.ant/lib  \
             -Dreactor.repo=file://${HOME}/.m2/repository \
-            -DskipRegexChecksum=.*-cdh5\..* \
+            -DskipRegexChecksum=.*-cdh6\..* \
             -DuseLocalJavadocUrl=true"
 
 ant $BUILD_OPTS ivy-bootstrap
